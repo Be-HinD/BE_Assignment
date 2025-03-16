772ecf20ce68f22b71ea6aaf5e3f22599d53acce
Be-HinD <kr100475@gmail.com>
2025-03-16 22:00:07 +0900

    [chore] DB Settings

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,13 +1,21 @@
+# app/api/routes/users.py
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database.session import SessionLocal
+from sqlalchemy import text
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 router = APIRouter(prefix="/users", tags=["users"])
 
-@router.get(
-    "/",
-    # dependencies=[Depends(get_current_active_superuser)],
-    # response_model=UsersPublic,
-)
-def read_users():
-    
-    return "Hello"
+@router.get("/")
+def read_users(db: Session = Depends(get_db)):
+
+    # 예시 : 직접 SQL문 실행
+    result = db.execute(text("SELECT * FROM users")).fetchall()
+    return {"users": [dict(row) for row in result]}
--- app/main.py
+++ app/main.py
@@ -1,19 +1,34 @@
-
+# app/main.py
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
-# from app.core.config import settings
-
+from app.database.base import Base
+from app.database.session import engine
 
 def custom_generate_unique_id(route: APIRoute) -> str:
     return f"{route.tags[0]}-{route.name}"
 
 app = FastAPI(
-    title="Assigment",
-    # openapi_url=f"Assigment",
+    title="Assignment",
     generate_unique_id_function=custom_generate_unique_id,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router, prefix="/v1")
+
+# 애플리케이션 시작 시 모델을 임포트한 후 테이블 생성
+@app.on_event("startup")
+def on_startup():
+    # 모델 임포트
+    import app.models.user
+
+    Base.metadata.create_all(bind=engine)
