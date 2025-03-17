d703a93d06ba93d1d0df1fe69a96ca14b7c1274c
Be-HinD <kr100475@gmail.com>
2025-03-17 19:35:52 +0900

    [feat] admin_reservation router 등록

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,10 @@
 # app/api/main.py (일부)
 from fastapi import APIRouter
-from app.api.routes.admin import admin
+from app.api.routes.admin import admin_reservation
 from app.api.routes.user import users, token, reservation  # token 모듈 추가
 
 api_router = APIRouter()
 api_router.include_router(users.router)            # /v1/users/...
-api_router.include_router(admin.router)
+api_router.include_router(admin_reservation.router)
 api_router.include_router(token.router)            # /v1/token
 api_router.include_router(reservation.router)
